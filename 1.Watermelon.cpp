39453d0fe1e74a4c8e6c52dd73d1715b644b1d60
Mugdha-Hazra <63424869+Mugdha-Hazra@users.noreply.github.com>
2020-11-21 13:03:53 +0530

    Create 1.Watermelon.cpp

---
+++ 1.Watermelon.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/problemset/problem/4/A
+#include<iostream>
+using namespace std;
+int main()
+{int n;
+cin>>n;
+if((n%2==0)&&(n>2))
+cout<<"YES";
+else
+cout<<"NO";                   
+return 0;
+}
